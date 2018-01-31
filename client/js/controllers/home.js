fffae5996dfe8c5544eeb57c3ab9992eb7ab8592
Anindyasundar Bera <anindya@cryptonsoftech.com>
2018-01-31 15:32:12 +0530

    init

---
+++ client/js/controllers/home.js
@@ -0,0 +1,33 @@
+angular
+  .module('app')
+  .controller('HomeController', ['$scope', 'AuthService', '$state',
+      function($scope, AuthService, $state) {
+    $scope.user = {
+      email: 'foo@bar.com',
+      password: 'foobar'
+    };
+
+    // $scope.title = "HireMe | Home";
+
+    $scope.login = function() {
+      AuthService.login($scope.user.email, $scope.user.password)
+        .then(function() {
+
+          // return to saved returnTo state before redirection to login
+          if ($scope.returnTo && $scope.returnTo.state) {
+            $state.go(
+              $scope.returnTo.state.name,
+              $scope.returnTo.params
+            );
+            // maintain the inherited rootscope variable returnTo
+            // but make the returnTo state of it null,
+            // so it can be used again after a new login.
+            $scope.returnTo.state  = null;
+            $scope.returnTo.params = null;
+            return;
+          }
+          // or go to the default state after login
+          $state.go('add-review');
+        });
+    };
+  }]);
